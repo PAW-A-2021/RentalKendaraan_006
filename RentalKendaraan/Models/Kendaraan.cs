010b9577f4710bf1ca260fae82dcb375f91ec434
Sisilia Indah <sisilia.indah.ft19@mail.umy.ac.id>
2021-12-01 17:30:56 +0700

    Praktikum 8

--- RentalKendaraan/Models/Kendaraan.cs
+++ RentalKendaraan/Models/Kendaraan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,10 +14,18 @@ namespace RentalKendaraan.Models
         }
 
         public int IdKendaraan { get; set; }
+
+        [Required(ErrorMessage = "Nama Kendaraan Tidak Boleh Kosong")]
         public string NamaKendaraan { get; set; }
+
+        [Required(ErrorMessage = "No Polisi Tidak Boleh Kosong")]
         public string NoPolisi { get; set; }
+
+        [Required(ErrorMessage = "No Stnk Tidak Boleh Kosong")]
         public string NoStnk { get; set; }
         public int? IdJenisKendaraan { get; set; }
+
+        [Required(ErrorMessage = "Ketersediaan Tidak Boleh Kosong")]
         public string Ketersediaan { get; set; }
 
         public virtual JenisKendaraan IdJenisKendaraanNavigation { get; set; }
